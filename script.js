8d567d82b53706db45ac78899ea9f9504bcc9fd4
gantengam88-ops <gantengam88@gmail.com>
2026-06-30 20:36:50 +0700

    Add button click functionality to display message

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("script jalan");
+const btn = document.getElementById("giftBtn");
+
+if(btn){
+
+btn.onclick=function(){
+
+document.querySelector(".hero").innerHTML=`
+
+<div class="card">
+
+<h2>💌 BERHASIL</h2>
+
+<p>
+Sekarang tombol sudah bekerja.
+</p>
+
+</div>
+
+`;
+
+};
+
+}
