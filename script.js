8d2736bb2c14e5b941f3afa988181869c968e309
gantengam88-ops <gantengam88@gmail.com>
2026-06-30 19:43:54 +0700

    Update script.js to display birthday message

--- script.js
+++ script.js
@@ -7,15 +7,39 @@ document.body.style.transition =
 "1s";
 
 document.body.style.opacity =
-".2";
+"0";
 
 setTimeout(()=>{
 
-alert(
-"Part 2 akan membuka hadiah 💌"
-);
+document.body.innerHTML=`
 
-document.body.style.opacity =
+<div class="letter-page">
+
+<div class="letter">
+
+<h2>💌 Untuk Kamu</h2>
+
+<p>
+Selamat ulang tahun ya ❤️
+<br><br>
+Terima kasih sudah hadir,
+nemenin, dan jadi bagian
+dari banyak cerita.
+<br><br>
+Ini baru awal dari hadiah kecil ini ✨
+</p>
+
+<button onclick="location.reload()">
+💖 Kembali
+</button>
+
+</div>
+
+</div>
+
+`;
+
+document.body.style.opacity=
 "1";
 
 },900);
